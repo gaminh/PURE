4c8b0df1862824cadcbac78ecbd910ce19bc150a
Tuan-Minh Nguyen <30328881+gaminh@users.noreply.github.com>
2023-07-13 22:43:38 -0500

    Combining all the scripts into one script. Uploading IPA results.

--- PURE_v3.R
+++ PURE_v3.R
@@ -1,24 +1,50 @@
 setwd("~/PURE-main")
 options("scipen"=100, "digits"=4)
+
+# Installing packages ------------------------------------------------------
+# If the packages have already been installed, please skip this section
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("fgsea")
+install.packages("ggplot2")
+install.packages("plotrix")
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("xlsx")
+
+
+# Loading packages --------------------------------------------------------
+
+library(fgsea)
 library(ggplot2)
 library("plotrix")
 library(tidyverse)
+library("readxl")
+# library("xlsx")
+
+# Benchmarking datasets ---------------------------------------------------
+
 
 datasets = c("exp1_GSE26487", "exp2_GSE49804", "exp3_GSE86837", "exp4_GSE58434",
                           "exp5_GSE58434", "exp6_GSE11352", "exp7_GSE11352", "exp8_GSE11352", 
                           "exp9_GSE74000", "exp10_GSE12446", "exp11_GSE67266", "exp12_GSE67266", 
-                          "exp13_GSE51213", "exp14_GSE58875")
+                          "exp13_GSE51213", "exp14_GSE58875", "exp15_GSE147507", "exp16_GSE147507")
 
 targetChemical = c("Dexamethasone", "Dexamethasone", "Diethylhexyl Phthalate", "Calcitriol",
                    "Calcitriol", "Estradiol", "Estradiol", "Estradiol", "Acetaminophen",
-                   "Estradiol", "Etoposide", "Etoposide", "Dexamethasone", "Copper deficiency")
+                   "Estradiol", "Etoposide", "Etoposide", "Dexamethasone", "Copper deficiency", "Methylprednisolone", "Methylprednisolone")
 
 
 GEOID = c("GSE26487", "GSE49804", "GSE86837", "GSE58434", "GSE58434", "GSE11352", "GSE11352", "GSE11352", 
-          "GSE74000", "GSE12446", "GSE67266", "GSE67266", "GSE51213", "GSE58875")
+          "GSE74000", "GSE12446", "GSE67266", "GSE67266", "GSE51213", "GSE58875", "GSE147507", "GSE147507")
 
 organism = c("Human", "Mouse", "Mouse", "Human", "Human", "Human", "Human", "Human", "Human", "Human", "Mouse",  
-             "Mouse", "Mouse", "Rat")
+             "Mouse", "Mouse", "Rat", "Human", "Human")
+
+Hypothesis = c(rep("H1", 14), rep("H2", 2))
+
+# Defining helping functions to load datasets and knowledge base -------------------
 
 create_geneset <- function(network)
 {
@@ -36,7 +62,10 @@ getInputData <- function(identifier, cacheFolder="./data", suffix){
   return(inputData)
 }
 
-run_PURE <- function(gene_sets, gene_set_names, network = network, seed=1) {
+
+# Running PURE algorithm ------------------------------------------------------
+
+run_PURE <- function(gene_sets, gene_set_names, network = network, seed=1, Hypothesis = "H1") {
   set.seed(seed)
   
   res <- lapply(gene_set_names, function(gsn){
@@ -44,25 +73,23 @@ run_PURE <- function(gene_sets, gene_set_names, network = network, seed=1) {
     
     DEGenes <- unique(network$GeneID[network$sign == 1 & network$ChemicalID == gsn])
     
-    wBallDraw <- length(DEGenes) - 1
-    if (wBallDraw < 0) return(1)
     
-    wBall <- length(unique(network$GeneID[network$sign == 1]))
-    bBall <- length(unique(network$GeneID[network$sign != 1]))
-    ballDraw <- length(gs)
+    l = length(DEGenes)
+    k = length(gs) - l
+    m = length(unique(network$GeneID[network$sign == 1])) - l
+    n = length(unique(network$GeneID[network$sign != 1])) - k
     
-    phyper(wBallDraw, wBall, bBall, ballDraw, lower.tail = F)
-    
-    ## One can use fisher.test as follows:
-    # fisher.test(matrix(c(length(DEGenes), ballDraw - length(DEGenes), 
-    #                     wBall - length(DEGenes), 
-    #                     bBall - ballDraw + length(DEGenes)), nrow = 2), 
-    #            alternative = "greater")$p.value
+    if(Hypothesis == "H1") {  
+      p = fisher.test(matrix(c(l,k,m,n), nrow = 2), alternative = "greater")$p.value
+    } else {
+      p = fisher.test(matrix(c(k,l,n,m), nrow = 2), alternative = "greater")$p.value
+    }
+    p
   }) %>% unlist() %>% data.frame(stringsAsFactors = F)
   
   colnames(res) <- 'p.value'
   res$padj <- p.adjust(res$p.value, "fdr")
-  res$pathway <- gene_set_names
+  res$CDT <- gene_set_names
   res <- res %>% tidyr::drop_na()
   res
 }
@@ -90,19 +117,250 @@ for (i in 1:length(datasets)) {
 
   geneset <- create_geneset(network)
 
-  res <- run_PURE(geneset, names(geneset), network)
+  res <- run_PURE(geneset, names(geneset), network, Hypothesis = Hypothesis[i])
   
   tmp <- readRDS(paste0("CTD_", org, "_exp.rds"))
   tmp <- tmp[, 1:2]
   tmp <- tmp[!duplicated(tmp),]
   rownames(tmp) <- tmp$ChemicalID
   
-  res$pathway <- tmp[res$pathway,]$ChemicalName
+  res$CDT <- tmp[res$CDT,]$ChemicalName
   
   p_value <- res$padj
-  names(p_value) <- res$pathway
+  names(p_value) <- res$CDT
   print(rank(p_value, ties.method =  "min")[chem])
   print(sum(p_value < 0.01))
   
   saveRDS(res, file = paste0("./PURE_", datasets[i], "_vOrg.rds"))
 }
+
+
+# Running other benchmarking methods --------------------------------------
+
+run_KS_W <- function(method = "ks", geneSet, FC, gene_p, seed = 1){
+  set.seed(seed)
+  
+  test <- if (method == "ks") ks.test else wilcox.test
+  idx <- which(sapply(geneSet, function(gs) sum(gene_p[gs] < 0.05) > 5))
+  geneSet <- geneSet[idx]
+  
+  FC <- FC[unique(unlist(geneSet))]
+  
+  allGenes <- names(FC)
+  
+  res <- lapply(geneSet, function(gs){
+    
+    DEhit <- FC[allGenes[allGenes %in% gs]]
+    DEmiss <- FC[allGenes[!allGenes %in% gs]]
+    
+    if (length(DEhit) == 0 | length(DEmiss) == 0) return(NA)
+    
+    test(DEhit, DEmiss)$p.value
+  }) %>% unlist() %>% data.frame(CDT=names(geneSet), stringsAsFactors = F)
+  
+  colnames(res) <- c("p.value", "CDT")
+  res <- res %>% drop_na()
+  res$padj <- p.adjust(res$p.value, "fdr")
+  
+  res
+}
+
+run_ORA <- function(geneSet, gene_p, seed = 1)
+{
+  set.seed(seed)
+  DEGenes <- names(gene_p[gene_p < 0.05])
+  
+  res <- lapply(geneSet, function(gs){
+    
+    wBallDraw <- intersect(gs, DEGenes) %>%  length() - 1
+    if (wBallDraw < 0) return(1)
+    
+    wBall <- length(DEGenes)
+    bBall <- length(gene_p) - length(DEGenes)
+    ballDraw <- length(intersect(gs, names(gene_p)))
+    
+    1 - phyper(wBallDraw, wBall, bBall, ballDraw)
+    
+  }) %>% unlist() %>% data.frame(stringsAsFactors = F)
+  
+  colnames(res) <- 'p.value'
+  res$CDT = rownames(res)
+  res <- res %>% tidyr::drop_na()
+  res$padj <- p.adjust(res$p.value, "fdr")
+  
+  res
+}
+
+for (method in c("ORA", "KS", "Wilcox", "FGSEA")) { 
+  for (i in 1:length(datasets)) {
+    set.seed(1)
+    chem <- targetChemical[i]
+    org <- organism[i]
+    if(org == "Rat") org <- "Rattus"
+    
+    network <- readRDS(paste0("CTD_",org,"_exp.rds"))
+    # if(org == "Mouse") 
+    network <- network[,c("ChemicalID", "GeneID", "edge_type")]
+    
+    all_gene <- unique(network$GeneID)
+    
+    if (method == "FGSEA") {
+      data <- getInputData(identifier =  datasets[i], suffix = "ga_de.csv")
+    } else {
+      data <- getInputData(identifier =  datasets[i], suffix = "ga_all.csv")
+    }
+    
+    data$entrez <- as.character(data$entrez)
+    data <- data[data$entrez %in% all_gene, ]
+    
+    gene_fc <- data$logfc
+    gene_p <- data$adjpv
+    names(gene_fc) <- names(gene_p) <- data$entrez
+    
+    network <- network[network$GeneID %in% data$entrez, ]
+    
+    geneset <- create_geneset(network)
+    
+    if (method == "FGSEA") {
+      res <- fgsea(pathways = geneset, stats = gene_fc, nperm = 1e4, minSize = 15)
+      res <- data.frame(CDT = res$pathway, p.value = res$pval, padj = res$padj, stringsAsFactors = F)
+    }
+    if (method == "KS") {
+      res <- run_KS_W("ks", geneset, gene_fc, gene_p)
+    }
+    if (method == "Wilcox") {
+      res <- run_KS_W("wilcox", geneset, gene_fc, gene_p)
+    }
+    if (method == "ORA") {
+      res <- run_ORA(geneset, gene_p)
+    }
+    
+    tmp <- readRDS(paste0("CTD_", org, "_exp.rds"))
+    tmp <- tmp[, 1:2]
+    tmp <- tmp[!duplicated(tmp),]
+    rownames(tmp) <- tmp$ChemicalID
+    
+    res$CDT <- tmp[res$CDT,]$ChemicalName
+    
+    saveRDS(res, file = paste0("./", method, "_", datasets[i], "_vOrg.rds"))
+  }
+}
+
+
+# Collecting the results ---------------------------------------------
+
+methods <- c("PURE", "ORA", "KS", "Wilcox", "FGSEA", "IPA", "IPA_CDT")
+result_table <- matrix(nrow = length(datasets), ncol = length(methods)*3) 
+colnames(result_table) <- c(paste0("Rank_", methods), paste0("NrSignDrug_", methods), paste0("pAdj_", methods))
+rownames(result_table) <- datasets
+
+for (method in methods) {
+  for (i in 1:length(datasets)) {
+    print(paste0(method, "_", i))
+    set.seed(1)
+    chem <- targetChemical[i]
+    if(!grepl("IPA", method)) {
+      res <- readRDS(paste0("./", method, "_", datasets[i], "_vOrg.rds"))
+      p_value <- res$padj
+      names(p_value) <- res$CDT
+      result_table[datasets[i], paste0("Rank_",method)] <- rank(p_value, ties.method = c("average"))[chem]
+      result_table[datasets[i], paste0("NrSignDrug_",method)] <- sum(p_value< 0.05)
+      result_table[datasets[i], paste0("pAdj_",method)] <- res[res$CDT == chem, "padj"]
+    } else {
+      res <-  as.data.frame(read_xls(paste0("IPA_analysis_v3/IPA_", datasets[i], ".xls"), sheet = "Sheet1", guess_max = 21474836))
+      res <- res[!is.na(res$`Activation z-score`),] 
+      if(method == "IPA_CDT") {
+        res <- res[grepl("chemical|drug", res$`Molecule Type`),] # for IPA_CDT, filter only chemicals/drugs in molecule type
+      }
+      res <- res[,c("Upstream Regulator", "Activation z-score", "p-value of overlap")]
+      colnames(res) <- c("CDT", "z_score", "p_overlap")
+      res$CDT <- toupper(res$CDT)
+      z_score <- res$z_score
+      names(z_score) <- res$CDT
+      
+      chem <- case_when(i == 3 ~ "di(2-ethylhexyl) phthalate",
+                        i %in% c(6, 7, 8, 10) ~ "beta-estradiol",
+                        TRUE ~ chem)
+      
+      if (toupper(chem) %in% names(z_score)) {
+        if(i == 15 | i ==16){
+          result_table[datasets[i], paste0("Rank_",method)] <- rank(z_score, ties.method = c("average"))[toupper(chem)]
+        } else {
+          result_table[datasets[i], paste0("Rank_",method)] <- rank(-z_score, ties.method = c("average"))[toupper(chem)]
+        }
+        result_table[datasets[i], paste0("pAdj_",method)] <- res[res$CDT == toupper(chem), "p_overlap"]
+      }
+      if(i == 15 | i ==16){
+        result_table[datasets[i], paste0("NrSignDrug_",method)] <- sum(z_score <= -2)
+      } else {
+        result_table[datasets[i], paste0("NrSignDrug_",method)] <- sum(z_score >= 2)
+      }
+    }
+  }
+}
+
+write.csv(result_table, file = "Result_Table.csv", row.names = T)
+#Summary for H1
+avg <- round(apply(result_table[c(1:14),], 2, mean, na.rm = T),1)
+med <- round(apply(result_table[c(1:14),], 2, median, na.rm = T),1)
+sd <-round(apply(result_table[c(1:14),], 2, sd, na.rm = T),1)
+summary <- paste0(avg,"+/-" ,sd)
+names(summary) <- c(paste0("Rank_",methods),paste0("Nr_Sig_",methods), paste0("pv",methods))
+print(summary)
+#Summary for H2
+round(apply(result_table[c(15,16),], 2, mean, na.rm = T),1)
+
+PURE_r = result_table[,"Rank_PURE"]
+
+index = is.na(IPA_CDT_r)
+IPA_CDT_r[index] = result_table[index, "NrSignDrug_IPA_CDT"] + 1
+IPA_r[index] = result_table[index, "NrSignDrug_IPA"] + 1
+
+wilcox.test(PURE_r, IPA_CDT_r, alternative = "less")
+wilcox.test(PURE_r, IPA_r, alternative = "less")
+wilcox.test(result_table[,"Rank_PURE"], result_table[,"Rank_ORA"], alternative = "less")
+wilcox.test(result_table[,"Rank_PURE"], result_table[,"Rank_KS"], alternative = "less")
+wilcox.test(result_table[,"Rank_PURE"], result_table[,"Rank_Wilcox"], alternative = "less")
+wilcox.test(result_table[,"Rank_PURE"], result_table[,"Rank_FGSEA"], alternative = "less")
+
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_IPA"], alternative = "less")
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_IPA_CDT"], alternative = "less")
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_ORA"], alternative = "less")
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_KS"], alternative = "less")
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_FGSEA"], alternative = "less")
+wilcox.test(result_table[,"NrSignDrug_PURE"], result_table[,"NrSignDrug_Wilcox"], alternative = "less")
+
+rankResult <- list(PURE = result_table[,"Rank_PURE"], 
+                   ORA = result_table[,"Rank_ORA"], 
+                   KS = result_table[,"Rank_KS"], 
+                   Wilcoxon = result_table[,"Rank_Wilcox"], 
+                   FGSEA = result_table[,"Rank_FGSEA"], 
+                   IPA = result_table[,"Rank_IPA"], 
+                   `IPA-CDT` = result_table[,"Rank_IPA_CDT"])
+rankResult <- lapply(rankResult, FUN = log) 
+
+pdf("RankTarget_vOrg_log_v3.pdf", width = 8, height = 8)
+gap.boxplot(rankResult, gap=list(top=c(NA,NA),bottom=c(NA,NA)),
+            col=c("firebrick3", "darkorchid1", "cornflowerblue", "chartreuse3", "orange", "#69b3a2", "#488c7c"))
+axis(2, labels=c(0), at=c(0))
+title(ylab="log(ranks) of the true CDTs", xlab= "Method", col="black")
+dev.off()
+
+# Nr of signficant drugs reported
+
+fpResult <- list(PURE = result_table[,"NrSignDrug_PURE"], 
+                 ORA = result_table[,"NrSignDrug_ORA"], 
+                 KS = result_table[,"NrSignDrug_KS"], 
+                 Wilcoxon = result_table[,"NrSignDrug_Wilcox"], 
+                 FGSEA = result_table[,"NrSignDrug_FGSEA"], 
+                 IPA = result_table[,"NrSignDrug_IPA"], 
+                 `IPA-CDT` = result_table[,"NrSignDrug_IPA_CDT"])
+pdf("NrSigChem_vOrg_v3.pdf", width = 8, height = 8)
+
+gap.boxplot(fpResult, gap=list(top=c(NA,NA),bottom=c(NA,NA)),
+            col=c("firebrick3", "darkorchid1", "cornflowerblue", "chartreuse3", "orange", "#69b3a2", "#488c7c"))
+axis(2, labels=c(0), at=c(0))
+title(ylab="Number of significant CDTs reported", xlab= "Method", col="black")
+
+dev.off()
+
---
+++ README.md
@@ -0,0 +1,37 @@
+1. Introduction: External factors such as exposure to a chemical, drug, or toxicant (CDT), or conversely, the lack of certain chemicals can cause many diseases. The ability to identify such causal CDTs based on changes in the gene expression profile is extremely important in many studies. Furthermore, the ability to correctly infer CDTs that can revert the gene expression changes induced by a given disease phenotype is a crucial step in drug repurposing.  
+We present an approach for Predicting Upstream REgulators (PURE) designed to tackle this challenge. PURE can correctly infer a CDT from the measured expression changes in a given phenotype, as well as correctly identify drugs that could revert disease-induced gene expression changes.
+
+This script, "PURE_v3.R” implements the PURE algorithm and also compare with four classical approaches as well as with the causal analysis used in Ingenuity Pathway Analysis (IPA) on 16 data sets (1 rat, 5 mouse, and 10 human data sets), involving 8 chemicals or drugs.
+
+2. Configuration: This script was created using RStudio version 2023.06.0+421 (2023.06.0+421), R version 4.3.0 (2023-04-21), under macOS Monterey 12.0.1. The following packages and versions are needed for running the script:
+
+readxl, Version 1.4.2
+lubridate, Version 1.9.2
+forcats, Version 1.0.0
+stringr, Version 1.5.0
+dplyr, Version 1.1.2
+purrr, Version 1.0.1
+readr, Version 2.1.4
+tidyr, Version 1.3.0
+tibble, Version 3.2.1
+tidyverse, Version 2.0.0    
+plotrix, Version 3.8-2
+ggplot2, Version 3.4.2
+fgsea, Version 1.26.0
+BiocManager, Version 1.30.21
+
+4. Inputs:
+- The knowledge base. We used The Comparative Toxicogenomics Database of three species, namely human, mouse, and rat for this study. The knowledge base contains the information about the interactions between CDTs and downstream genes.  It was downloaded from The Comparative Toxicogenomics Database website (https://ctdbase.org)
+- The gene expression profiles. Each data consists of 4 columns: the log(fold-change) of condition’s gene expression compared to control’s ones (column name: logfc), the FDR-adjusted p values of the changes in gene expressions between the condition and control samples (column name: adjpv), entrez ID of the gene (column name: entrez), and the gene symbol (column name: symbol). While some methods, including PURE, only require the information of the DE genes (files with suffix “_ga_de”), others need the information of all the genes measured (files with suffix “_ga_all”). All the gene expression profiles from 16 experiments included in the study are saved in the “data” folder.
+- IPA’s results. The results from IPA were obtained directly from IPA software with default settings and stored in "IPA_analysis_v3" folder.
+
+3. Structure: The script consists of 7 sections:
+- Installing the packages. 
+- Loading packages. 
+- Benchmarking datasets. The vectors that contain the information about the data sets are defined in this section, including the dataset name, target CDT, GEO ID, organism, and hypothesis testing for each experiment.
+- Defining helping functions to load datasets and knowledge base. The function “create_geneset” creates a list of downstream genes for each CDT for a given knowledge base. The function “getInputData” loads the gene expression profiles into the working environment.
+- PURE function (run_PURE). The function takes the following inputs: the sets of downstream genes of the CDTs in the knowledge base (using the “create_geneset” function), the name of the CDTs, the knowledge base, and the hypothesis testing which is either “H1” (default) or H2. The function will return p values and FDR-adjusted p values of the CDTs for the corresponding hypothesis. 
+- Implementing other benchmarking methods. The function “run_KS_W” implements the Kolmogorov-Smirnov test (KS) and Wilcoxon test. The parameter “method” takes either the value “ks” for KS or “wilcox” for Wilcoxon test. The other parameters for “run_KS_W” functions are the list of downstream genes for each CDT, the fold-changes, and the p values of the genes. The function “run_ORA” implements the ORA approach and takes the downstream gene sets, and p values of the genes as the parameters. For FGSEA we used the function “fgsea” from the fgsea package. We did not implemented IPA but obtained its results directly from IPA software.
+- Collecting the results. The results from running each algorithm are saved in the format of “<method>_<datasets>_vOrg.rds". All the results will be saved in the variable “result_table” which is then saved as "Result_Table.csv" in the same folder. Some statistics mentions in the manuscript such as the average values, the standard deviations, the p value when comparing the performance of PURE and other methods are also calculated in this section. Lastly, we included the code that generate the figures in the manuscript.
+
+Contact: Please contact tuanminh22@gmail.com if you have any question.
